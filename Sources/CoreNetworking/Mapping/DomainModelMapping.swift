18f2dbd0266cb75e1d00387a4258d737ce6c5d73
Brayden Harris <brayden4012@gmail.com>
2025-06-25 20:37:43 -0500

    open DomainModelMapping

--- Sources/CoreNetworking/Mapping/DomainModelMapping.swift
+++ Sources/CoreNetworking/Mapping/DomainModelMapping.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public class DomainModelMapping: Hashable {
+open class DomainModelMapping: Hashable {
     func networkType() -> NetworkModel.Type {
         fatalError("Must override in subclass")
     }
